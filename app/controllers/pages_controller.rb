191e6686be07252c0173393c25e6b4af7571a205
Tim Pile <tim@gritchat.com>
2018-03-10 16:53:15 -0500

    Added token field to users, attempt to connect to api and retrieve activities for user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @client = Strava::Api::V3::Client.new(:access_token => current_user.token) if current_user.token
+    @activities = @client.list_athlete_activities if @client
   end
 end
