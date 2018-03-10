7e409fc61341dfdbe1f801c71528565c0f7d4abb
Tim Pile <tim@gritchat.com>
2018-03-10 17:02:53 -0500

    Updated user model auth to get token from credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
 
  def self.from_omniauth(auth)
    puts auth
+   puts auth.credentials
+   puts auth.info
    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
      user.email = auth.info.email
      user.password = Devise.friendly_token[0,20]
-     user.token = auth.access_token
+     user.token = auth.credentials.access_token
      # user.name = auth.info.name   # assuming the user model has a name
      # user.image = auth.info.image # assuming the user model has an image
    end
