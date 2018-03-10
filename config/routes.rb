5906ba164cbef47459301080892a72aa0a123669
Tim Pile <tim@gritchat.com>
2018-03-10 16:41:48 -0500

    Updated routes and views for ability to logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get '/auth/:provider/callback', to: 'sessions#create'
+  devise_for :users,
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'register'
+    },
+    controllers: {
+      omniauth_callbacks: 'users/omniauth_callbacks'
+    }
   root to: "pages#home"
 end
