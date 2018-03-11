f487a6eeaab716e2e785150ef49ff3dd533a5d2e
Tim Pile <tim@gritchat.com>
2018-03-11 17:45:23 -0400

    Created helper for unit conversions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def conversion_helper from, to, value = 1
+    if from == "meters" && to == "miles"
+      (value * 0.000621371).round(1)
+    elsif from == "seconds" && to == "minutes"
+      (value / 60.0).round(1)
+    elsif from == "mps" && to == "pace"
+      (26.8224 / value).round(1)
+    end
+  end
+
 end
