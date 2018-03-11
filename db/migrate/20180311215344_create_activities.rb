20b376d65f13f2bcb6050911e67c154d159b6b0e
Tim Pile <tim@gritchat.com>
2018-03-11 17:56:05 -0400

    Generated activities model, migrated database

---
+++ db/migrate/20180311215344_create_activities.rb
@@ -0,0 +1,17 @@
+class CreateActivities < ActiveRecord::Migration[5.1]
+  def change
+    create_table :activities do |t|
+      t.references :user, foreign_key: true
+      t.bigint :strava_id
+      t.string :name
+      t.string :activity_type
+      t.string :string
+      t.timestamp :start_date_local
+      t.integer :elapsed_time
+      t.text :description
+      t.integer :distance
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310214716) do
+ActiveRecord::Schema.define(version: 20180311215344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "strava_id"
+    t.string "name"
+    t.string "activity_type"
+    t.string "string"
+    t.datetime "start_date_local"
+    t.integer "elapsed_time"
+    t.text "description"
+    t.integer "distance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,4 +50,5 @@ ActiveRecord::Schema.define(version: 20180310214716) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users"
 end
